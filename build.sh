6b9c896d27b0a1bb4957d0c3d2dc979784c21d5e
Vishal <vishalathmanathan@gmail.com>
2024-07-16 09:53:41 +0530

    push v4

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ fi
 if ! command -v pip &> /dev/null; then
     echo "pip could not be found, installing pip..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
+    python3.9 get-pip.py
     rm get-pip.py
 fi
 
